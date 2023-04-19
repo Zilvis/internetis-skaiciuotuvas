727c0005b6273d921dcbd26b39d5f01c303c136d
zilvis <zi.daugela@gmail.com>
2023-04-19 18:32:37 +0300

    pirmas commit

---
+++ src/main/java/com/skaiciuotuvas/Skaiciuotuvas/SkaiciuotuvasApplication.java
@@ -0,0 +1,13 @@
+package com.skaiciuotuvas.Skaiciuotuvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaiciuotuvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaiciuotuvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skaiciuotuvas/Skaiciuotuvas/SkaiciuotuvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaiciuotuvas.Skaiciuotuvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaiciuotuvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
